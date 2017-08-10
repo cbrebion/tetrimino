567903fad0cc3382901f719c90b435863a67fb34
Unknown <charles.brebion@gmail.com>
2017-08-10 14:32:31 +0200

    Modif Display

---
+++ src/com/sopra/servlets/DisplayPlayerServlet.java
@@ -0,0 +1,20 @@
+package com.sopra.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/listeJoueurs")
+public class DisplayPlayerServlet extends HttpServlet {
+	public static final String VUE_GET		= "/WEB-INF/admin/listeJoueurs.jsp";
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(VUE_GET).forward(req, resp);
+	}
+
+}
--- src/com/sopra/servlets/DisplayTetriminoServlet.java
+++ src/com/sopra/servlets/DisplayTetriminoServlet.java
@@ -1,7 +1,31 @@
 package com.sopra.servlets;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sopra.dao.ITetriminoDAO;
+import com.sopra.dao.server.TetriminoServerDAO;
+import com.sopra.model.Tetrimino;
+
+@WebServlet("/admin/listeTetriminos")
 public class DisplayTetriminoServlet extends HttpServlet {
+	public static final String VUE_GET		= "/WEB-INF/admin/afficherTetriminos.jsp";
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ITetriminoDAO tetriminoServerDAO = new TetriminoServerDAO();
+		
+		List<Tetrimino> tetriminos = tetriminoServerDAO.rechercher(req);
+		
+		req.setAttribute("tetriminos", tetriminos);
+		
+		this.getServletContext().getRequestDispatcher(VUE_GET).forward(req, resp);
+	}
 
 }
