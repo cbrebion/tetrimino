4c680191e4750f135e59e3383ec91bcb54faad67
Unknown <charles.brebion@gmail.com>
2017-08-10 12:21:06 +0200

    Modif HomeServlet / ajout AdminHomeServlet

--- src/com/sopra/servlets/HomeServlet.java
+++ src/com/sopra/servlets/HomeServlet.java
@@ -10,17 +10,30 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
-	private static final String VUE_GET		= "/WEB-INF/accueil.jsp";
-	private static final String VUE_POST	= "/accueilAdmin";
+	private static final String VUE_GET		= "/WEB-INF/Accueil.jsp";
+	private static final String VUE_POST	= "accueilAdmin";
+	
+	private static final String CHAMP_USER	= "username";
+	private static final String CHAMP_PASS	= "pass";
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher(VUE_GET).forward(req, resp);
+		if (req.getSession().getAttribute(CHAMP_USER) == null) {
+			this.getServletContext().getRequestDispatcher(VUE_GET).forward(req, resp);
+		} else {
+			resp.sendRedirect(VUE_POST);
+		}
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter(CHAMP_USER);
+		String pass = req.getParameter(CHAMP_PASS);
 		
+		if (username.equals("admin") && pass.equals("admin")) {
+			req.getSession().setAttribute("username", username);
+			req.getSession().setAttribute("pass", pass);
+		}
+		this.doGet(req, resp);
 	}
-
 }
