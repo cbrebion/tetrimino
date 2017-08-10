ae09a2bd1a06f6b5f6f68e7a72b0bdfaedaeb2d6
Unknown <charles.brebion@gmail.com>
2017-08-10 12:04:07 +0200

    Mise à jour HomeServlet

--- src/com/sopra/servlets/HomeServlet.java
+++ src/com/sopra/servlets/HomeServlet.java
@@ -1,7 +1,26 @@
 package com.sopra.servlets;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
 public class HomeServlet extends HttpServlet {
+	private static final String VUE_GET		= "/WEB-INF/accueil.jsp";
+	private static final String VUE_POST	= "/accueilAdmin";
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(VUE_GET).forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
 
 }
