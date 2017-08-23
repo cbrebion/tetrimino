7510f9d7959ecc43d5cfeea50f9a6b5ab8e3f74b
gonzagueducrocq <gonzague.ducrocq@soprasteria.com>
2017-08-23 12:16:48 +0200

    homeJoueurServlet

--- src/com/sopra/servlets/HomeJoueurServlet.java
+++ src/com/sopra/servlets/HomeJoueurServlet.java
@@ -1,19 +1,32 @@
 package com.sopra.servlets;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sopra.dao.IPartieDAO;
+import com.sopra.model.Partie;
+
 @WebServlet("/accueilJoueur")
 public class HomeJoueurServlet extends HttpServlet {
 	private static final String VUE_ACCUEIL		= "/WEB-INF/accueilJoueur.jsp";
 
+	@EJB(name="partieHibernateDAO")
+	private IPartieDAO partieHibernateDAO;
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		List<Partie> parties = partieHibernateDAO.findAllDesc();
+		
+		req.setAttribute("parties", parties);
+
 		this.getServletContext().getRequestDispatcher(VUE_ACCUEIL).forward(req, resp);
 	}
 
